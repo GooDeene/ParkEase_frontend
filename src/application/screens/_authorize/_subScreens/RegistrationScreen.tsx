3ff023959b35d1729b86645dc25485bd213bc071
Владислав Медведевский <miedviedievskii@mail.ru>
2025-05-26 21:58:37 +0500

    fix build

--- src/application/screens/_authorize/_subScreens/RegistrationScreen.tsx
+++ src/application/screens/_authorize/_subScreens/RegistrationScreen.tsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-import './RegistrationScreen.css';
 import { isRequired } from '../../../../controls/_input/validators/isRequired';
 import { isValidRussianPhone } from '../../../../controls/_input/validators/isValidRussianPhone';
 import { isValidPassword } from '../../../../controls/_input/validators/isValidPassword';
